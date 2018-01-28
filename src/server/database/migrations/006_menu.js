d12bc4780dbb3cadc6d4676e38876803f0713de7
peepitkm <pornpat.pp@gmail.com>
2018-01-28 12:32:19 +0700

    Update menu migration

--- src/server/database/migrations/006_menu.js
+++ src/server/database/migrations/006_menu.js
@@ -24,10 +24,36 @@ exports.up = function(knex, Promise) {
 			table.string('title');
 			table.string('description');
 			table.decimal('rating', 3, 2);
+			//
+			table.integer('free_items').unsigned();
+			table.integer('sale_items').unsigned();
+			//
 			table.boolean('is_active').defaultTo(true);
 			table.timestamps(false, true);
 		}),
-		knex.schema.createTable('review', table => {
+		knex.schema.createTable('menu_schedule', table => {
+			table.increments();
+			table
+				.integer('menu_id')
+				.unsigned()
+				.references('id')
+				.inTable('menu')
+				.onDelete('CASCADE');
+			table.enu('type', ['only', 'every', 'any']);
+			table.date('schedule');
+		}),
+		knex.schema.createTable('menu_payment', table => {
+			table.increments();
+			table
+				.integer('menu_id')
+				.unsigned()
+				.references('id')
+				.inTable('menu')
+				.onDelete('CASCADE');
+			table.enu('type', ['only', 'every', 'any']);
+			table.string('object');
+		}),
+		knex.schema.createTable('menu_price', table => {
 			table.increments();
 			table
 				.integer('menu_id')
@@ -36,21 +62,49 @@ exports.up = function(knex, Promise) {
 				.inTable('menu')
 				.onDelete('CASCADE');
 			table
-				.integer('user_id')
+				.integer('unit_id')
 				.unsigned()
 				.references('id')
-				.inTable('user')
+				.inTable('menu_unit')
 				.onDelete('CASCADE');
-			table.string('content');
-			table.boolean('is_publish').defaultTo(true);
+			table.decimal('price', 6, 2);
+		}),
+		knex.schema.createTable('menu_unit', table => {
+			table.increments();
+			table.string('name');
+			table.string('description');
+			//
+			table.boolean('is_active').defaultTo(true);
 			table.timestamps(false, true);
+		}),
+		knex.schema.createTable('menu_appointment', table => {
+			table.increments();
+			table
+				.integer('menu_id')
+				.unsigned()
+				.references('id')
+				.inTable('menu')
+				.onDelete('CASCADE');
+			table.string('name');
+			table.string('short_description');
+			table.string('time');
+			//
+			table.integer('province_id').unsigned();
+			table.integer('district_id').unsigned();
+			table.integer('subdistrict_id').unsigned();
+			table.decimal('latitude', 10, 8);
+			table.decimal('longitude', 11, 8);
 		})
 	]);
 };
 
 exports.down = function(knex, Promise) {
 	return Promise.all([
-		knex.schema.dropTable('review'),
+		knex.schema.dropTable('menu_appointment'),
+		knex.schema.dropTable('menu_unit'),
+		knex.schema.dropTable('menu_price'),
+		knex.schema.dropTable('menu_payment'),
+		knex.schema.dropTable('menu_schedule'),
 		knex.schema.dropTable('menu'),
 		knex.schema.dropTable('category')
 	]);
